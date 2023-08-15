c7b51e4a27806791af0e620ea73cf0edffdf2c5f
Eighttits <eighttits2002@gmail.com>
2023-08-14 22:26:44 -0700

    arreglando boton

--- resources/views/forms/select-form.blade.php
+++ resources/views/forms/select-form.blade.php
@@ -73,7 +73,7 @@
                             <option value="{{ $form->id }}">{{ $form->title }}</option>
                         @endforeach
                     </select>
-                    <button type="submit" class="mt-4 mb-5 bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded">
+                    <button type="submit" class="mt-3 inline-flex items-center px-4 py-2 bg-blue-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-400 focus:bg-blue-400 active:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                         Ver Estadísticas
                     </button>
                 </form>
